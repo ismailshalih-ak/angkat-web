610cd6f3ea1ac41cd2b4ba7ddb16b8eda1aa42e7
Ismail Shalih Abdul Kholiq <ismailshalih@gmail.com>
2025-02-25 18:05:08 +0700

    move form to new page

---
+++ src/app/dashboard/personalize/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import ProfileForm from "@/components/profile-form";
+
+export default function Page() {
+  return (
+    <div>
+      <div className="max-w-4xl mx-auto p-6 space-y-8">
+        <ProfileForm />
+      </div>
+    </div>
+  );
+}
